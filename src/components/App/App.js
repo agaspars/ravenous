e7ca519513ef4fd16e4fddeedc22a1b4b9d768b9
agaspars <82602542+agaspars@users.noreply.github.com>
2022-12-19 15:32:27 +0200

    SearchBar feedback&setState, business data access

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './App.css';
+
 import BusinessList from '../Business/BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
 
 class App extends React.Component {
+
+  searchYelp(term,location,sortBy) {
+    console.log(`term ${term} Loc ${location} sort ${sortBy} `)
+  }
+
   render(){
     return (
     <div className="App">
       <h1>Pet Project</h1>
-      <SearchBar/>
-      <BusinessList/>
+      <SearchBar searchYelp={this.searchYelp}/>
+      <BusinessList businesses={businesses}/>
     </div>
   );
   }
