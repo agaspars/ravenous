44f66f83f38c0b8cd45c685be665fbe9d6e7f4f7
agaspars <82602542+agaspars@users.noreply.github.com>
2022-12-22 11:49:55 +0200

    Yelp API connected, Search by location&term

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,32 +3,44 @@ import './App.css';
 
 import BusinessList from '../Business/BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
+// const business = {
+//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
 
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ]
 
 class App extends React.Component {
 
   searchYelp(term,location,sortBy) {
-    console.log(`term ${term} Loc ${location} sort ${sortBy} `)
+    // console.log(`term ${term} Loc ${location} sort ${sortBy} `)
+    Yelp.search(term,location,sortBy).then(businesses=> {
+      this.setState({businesses: businesses})
+    })
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      businesses: [],
+    }
+    this.searchYelp=this.searchYelp.bind(this)
   }
 
   render(){
@@ -36,7 +48,7 @@ class App extends React.Component {
     <div className="App">
       <h1>Pet Project</h1>
       <SearchBar searchYelp={this.searchYelp}/>
-      <BusinessList businesses={businesses}/>
+      <BusinessList businesses={this.state.businesses}/>
     </div>
   );
   }
